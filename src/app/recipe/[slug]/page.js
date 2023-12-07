c1d34f56f021f7a1d9c340537b201c26d6ed3fcc
Rama <rmdhnprmd@gmail.com>
2023-12-08 00:32:25 +0700

    delete import

--- src/app/recipe/[slug]/page.js
+++ src/app/recipe/[slug]/page.js
@@ -2,47 +2,45 @@ import Image from "next/image";
 import { LiaClockSolid } from "react-icons/lia";
 import client from "@/utils/contentful";
 
-// const fetchServices = async (slug) => {
-//   try {
-//     const response = await client.getEntries({
-//       content_type: "recipe",
-//       limit: 1,
-//       "fields.slug": slug,
-//     });
-//     const item = response.items[0];
-//     // console.log("FetchServiceBlog", item.fields.method.content[0])
+const fetchServices = async (slug) => {
+  try {
+    const response = await client.getEntries({
+      content_type: "recipe",
+      limit: 1,
+      "fields.slug": slug,
+    });
+    const item = response.items[0];
+    // console.log("FetchServiceBlog", item.fields.method.content[0])
 
-//     const date = item.datePost;
-//     const datePost = new Intl.DateTimeFormat("en-US", {
-//       year: "numeric",
-//       month: "long",
-//       day: "numeric",
-//     }).format(date);
-    
-   
-//     return {
-//       datePost: datePost,
-//       title: item.fields.title,
-//       thumbnail: item.fields.thumbnail.fields.file.url,
-//       thumbnailAlt: item.fields.thumbnail.fields.title,
-//       featuredImage: item.fields.featuredImage.fields.file.url,
-//       featuredImageAlt: item.fields.featuredImage.fields.title,
-//       ingredients: item.fields.ingredients,
-//       method: item.fields.method.content,
-//       cookingTime: item.fields.cookingTime,
-//     };
-//   } catch (error) {
-//     console.error("Error fetching blog data:", error.message);
-//   }
-// };
+    const date = item.datePost;
+    const datePost = new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }).format(date);
+
+    return {
+      datePost: datePost,
+      title: item.fields.title,
+      thumbnail: item.fields.thumbnail.fields.file.url,
+      thumbnailAlt: item.fields.thumbnail.fields.title,
+      featuredImage: item.fields.featuredImage.fields.file.url,
+      featuredImageAlt: item.fields.featuredImage.fields.title,
+      ingredients: item.fields.ingredients,
+      method: item.fields.method.content,
+      cookingTime: item.fields.cookingTime,
+    };
+  } catch (error) {
+    console.error("Error fetching blog data:", error.message);
+  }
+};
 
 const Recipe = async ({ params }) => {
-  // const blogData = await fetchServices(params.slug);
+  const blogData = await fetchServices(params.slug);
 
   return (
     <article className="pt-40 pb-20 px-[8rem] bg-slate-100 text-slate-700">
-      Halo
-      {/* <div className="min-w-[600px] min-h-[600px]  rounded-full top-[30%] left-[50%] translate-x-[-50%] bg-gradient-to-r from-fuchsia-600 to-pink-600 absolute z-1 blur-[90px] "></div>
+      <div className="min-w-[600px] min-h-[600px]  rounded-full top-[30%] left-[50%] translate-x-[-50%] bg-gradient-to-r from-fuchsia-600 to-pink-600 absolute z-1 blur-[90px] "></div>
       <div className="bg-slate-300/40 py-20 rounded-2xl z-10 backdrop-blur-2xl relative">
         <div className="flex flex-col justify-center items-center text-center mx-auto w-1/2 mb-28">
           <h1 className="text-5xl font-extrabold mb-6">{blogData.title}</h1>
@@ -56,8 +54,8 @@ const Recipe = async ({ params }) => {
           <div className="max-w-fit mx-auto mb-16 ">
             <Image
               src={`https:${blogData.featuredImage}`}
-              width={700}
-              height={700}
+              width={500}
+              height={500}
               alt={blogData.featuredImageAlt}
               className=" rounded-xl w-auto h-auto"
               priority
@@ -71,34 +69,33 @@ const Recipe = async ({ params }) => {
               <LiaClockSolid size={"1.5em"} />
               <p>Cooking {blogData.cookingTime}m</p>
             </div>
-            <h2 className="text-xl font-semibold uppercase">
-              Ingridients:
-            </h2>
+            <h2 className="text-xl font-semibold uppercase">Ingridients:</h2>
 
             <div className="mb-8">
               <ul>
-                {
-                  blogData.ingredients.map((item, i) => (
-                    <li key={`list-${i}`} className="leading-loose list-disc ml-4">{item}</li>                 
-                  ))
-                }
+                {blogData.ingredients.map((item, i) => (
+                  <li
+                    key={`list-${i}`}
+                    className="leading-loose list-disc ml-4"
+                  >
+                    {item}
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
 
           <section>
             <figure>
-              {
-                blogData.method.map((item, i) => (
-                  <div key={`itemIndex-${i}`} className="mb-3">
-                    {item.content && item.content[0].value}
-                  </div>
-                ))
-              }
+              {blogData.method.map((item, i) => (
+                <div key={`itemIndex-${i}`} className="mb-3">
+                  {item.content && item.content[0].value}
+                </div>
+              ))}
             </figure>
           </section>
         </section>
-      </div> */}
+      </div>
     </article>
   );
 };
