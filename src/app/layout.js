bcf3a94f47893e7c5a3f5b90940c65b52a7615af
Rama <rmdhnprmd@gmail.com>
2023-12-07 23:41:55 +0700

    change some

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,11 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <SmoothScroll> */}
+        <SmoothScroll>
           <Navbar />
           {children}
           <Footer />
-        {/* </SmoothScroll> */}
+        </SmoothScroll>
       </body>
     </html>
   );
