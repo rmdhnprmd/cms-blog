5fe53bc01a2651fce8275ce7f06fc2289814d612
Rama <rmdhnprmd@gmail.com>
2023-12-10 02:10:10 +0700

    testing pakai axios

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Blog from "@/components/Blog.module";
 import Hero from "@/components/Hero.module";
-import { fetchServices } from "@/utils/contentfulService";
 
 export default async function Home() {
 
